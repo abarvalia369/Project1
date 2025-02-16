dbc45ec031f8e33aff2b3588366d711f1287aabd
Arpeet Barvalia <arpeetbarvalia@gmail.com>
2025-02-16 17:03:45 -0500

    edits to account.java

--- src/project1/Account.java
+++ src/project1/Account.java
@@ -3,10 +3,10 @@ package project1;
 /**
  Account class for RU Bank project.
  Represents a bank account with an account number, holder profile, and balance.
- Implements deposit and withdrawal operations overrides equals, toString, and compareTo.
+ Implements deposit and withdrawal operations.
  Overrides equals, toString, and compareTo.
 
- @author Arpeet
+ @author ArpeetBarvalia, Jonathan John
  */
 
 public class Account implements Comparable<Account> {
@@ -14,44 +14,77 @@ public class Account implements Comparable<Account> {
     private Profile       holder;
     private double       balance;
 
+    /**
+     * Initializes an Account Object with the following 3 parameters.
+     *
+     * @param number the account number for the new account object.
+     * @param holder the profile holder for the new account object.
+     * @param balance the account balance for the new account object.
+     */
     public Account(AccountNumber number, Profile holder, double balance) {
         this.number = number;
         this.holder = holder;
         this.balance = balance;
     }
+
+    /**
+     * A method to withdraw(subtract) money from the current account balance
+     *
+     * @param amount the amount to be withdrawn
+     */
+    public void withdraw(double amount) {
+        this.balance -= amount;
+    }
+
+    /**
+     * A method to deposit(add) money to the current account balance
+     *
+     * @param amount the amount to be deposited
+     */
+    public void deposit(double amount) {
+        this.balance += amount;
+    }
+
+
     public AccountNumber getNumber() {
-        return number;
+        return this.number;
+    }
+    public void setNumber(AccountNumber number){
+        this.number = number;
     }
     public Profile getHolder() {
-        return holder;
+        return this.holder;
+    }
+    public void setHolder(Profile holder){
+        this.holder = holder;
     }
     public double getBalance() {
-        return balance;
+        return this.balance;
+    }
+    public void setBalance(double balance){
+        this.balance = balance;
     }
 
     @Override
     public boolean equals(Object obj) {
-        if(this == obj) return true;
-        if(!(obj instanceof Account)) return false;
+        if( obj == null || !( obj instanceof Account ) ){
+            return false;
+        }
         Account other = (Account) obj;
         return this.number.equals(other.number);
     }
+
     @Override
     public String toString() {
 // Format: Account#[200017410] Holder[John Doe 2/19/2000] Balance[$600.00] Branch[BRIDGEWATER]
-        java.text.DecimalFormat df = new java.text.DecimalFormat(”$0.00”);
-        return “Account#[” + number.toString() + “] Holder[” + holder.toString() + “] Balance[”
-        + df.format(balance) + “] Branch[” + number.getBranch().toString() + “]”;
+        java.text.DecimalFormat df = new java.text.DecimalFormat("$0.00");
+        return "Account#[" + number.toString() + "] Holder[" + holder.toString() + "] Balance["
+        + df.format(balance) + "] Branch[" + number.getBranch().toString() + "]";
     }
     @Override
     public int compareTo(Account other) {
         return this.number.toString().compareTo(other.number.toString());
     }
 
-    public void withdraw(double amount) {
-        this.balance -= amount;
-    }
-    public void deposit(double amount) {
-        this.balance += amount;
-    }
+
 }
