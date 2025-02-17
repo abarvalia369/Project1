4cacdb6d630e475a9b25dca42b6f9d4857cce9a7
Arpeet Barvalia <arpeetbarvalia@gmail.com>
2025-02-17 06:15:19 -0500

    complete

--- src/project1/RunProject1.java
+++ src/project1/RunProject1.java
@@ -1,8 +1,12 @@
+package project1;
 
+/**
+ * A driver class to runs the software.
+ *
+ * @author arpeet barvalia, jonathan john
+ */
 public class RunProject1 {
     public static void main(String [] args) {
         new TransactionManager().run();
-
-
     }
 }
