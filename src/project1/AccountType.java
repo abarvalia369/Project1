fd85a516f71516bb9f77e6ccdefd7acc41783fb3
Arpeet Barvalia <arpeetbarvalia@gmail.com>
2025-02-17 04:34:50 -0500

    complete

--- src/project1/AccountType.java
+++ src/project1/AccountType.java
@@ -1,2 +1,25 @@
+package project1;
 
-private String code;
+/**
+ * An Enum class that defines the name of the account types with an additional property listed below.
+ * Account types: 01 checking, 02 regular savings, and 03 money market savings.
+ *
+ * @author arpeet barvalia, jonathan john
+ */
+public enum AccountType {
+
+    Checking("01"),
+    RegularSavings("02"),
+    MoneyMarketSavings("03");
+
+    private String code;
+
+    AccountType( String code ){
+        this.code = code;
+    }
+
+    public String getCode(){
+        return code;
+    }
+
+}
