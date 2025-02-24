69cf5fbb5e4815b48675ffeb57d7b95414ea72bb
Jonathan J. John <jjj116@ilab1.cs.rutgers.edu>
2025-02-24 13:59:51 -0500

    m

--- src/project1/TransactionManager.java
+++ src/project1/TransactionManager.java
@@ -33,8 +33,8 @@ public class TransactionManager {
                 continue;
             }
 
-            StringTokenizer token = new StringTokenizer( commandLine );
-            String command = token.nextToken();
+            StringTokenizer tokens = new StringTokenizer( commandLine );
+            String command = tokens.nextToken();
 
             switch (command){
                 case "O":
