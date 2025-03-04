688563a61de2362bf6134115ed050fe96138851f
jonathanJohn0405 <jonathanjohn0405@gmail.com>
2025-03-03 22:39:55 -0500

    "Activity javadoc"

--- src/project1/Activity.java
+++ src/project1/Activity.java
@@ -18,12 +18,25 @@ public class Activity implements Comparable<Activity> {
         this.atm = atm;
     }
 
+    /**
+     * Getter and Setter Methods for Activity
+     *
+     * @return variables of the constructor
+     */
     public Date getDate() {return date;}
     public Branch getLocation() {return location;}
     public char getType() {return type;}
     public double getAmount() {return amount;}
     public boolean atAtm() {return atm;}
 
+
+    /**
+     * Compares 2 Activities
+     *
+     * @param other the profile object to be compared.
+     * @return return 1 if this Activity object is greater than "other", return -1 if smaller;
+     * return 0 if they are equal.
+     */
     @Override
     public int compareTo(Activity other){
         int dateComparison = this.date.compareTo(other.date);
@@ -37,6 +50,11 @@ public class Activity implements Comparable<Activity> {
         return Character.compare(this.type, other.type); // Sort deposits before withdrawals if all else is equal
     }
 
+    /**
+     * Translates activity to a string
+     *
+     * @return returns String translation
+     */
     @Override
     public String toString(){
         return "Activity [Date: " + date + ", Location: " + location + ", Type: " + type + ", Amount: $" + String.format("%.2f", amount) + ", ATM: " + atm + "]";
